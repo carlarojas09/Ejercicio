5e70fed0cee8d3335dd15f54c66291900dc4fe88
Sala Golden <golden@academia.loc>
2023-03-31 17:12:06 -0500

    Primera parte

---
+++ src/Controlador.java
@@ -0,0 +1,29 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Controlador {
+   private JFrame ventana;
+   private Modelo modelo;
+   private Vista vista;
+
+   public Controlador() {
+      modelo = new Modelo(getImagen());
+      vista = new Vista(modelo.getImagen());
+      ventana = new JFrame("Dibujando Imagen");
+      ventana.add(vista);
+      ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      ventana.setSize(300, 300);
+      ventana.setVisible(true);
+   }
+
+   private Image getImagen() {
+	   ImageIcon icono = new ImageIcon("C:\\Users\\golden\\Pictures\\superman.jpg");
+      return icono.getImage();
+   }
+
+   public static void main(String[] args) {
+      new Controlador();
+   }
+}
---
+++ src/Modelo.java
@@ -0,0 +1,14 @@
+import java.awt.Image;
+
+public class Modelo {
+		   private Image imagen;
+
+		   public Modelo(Image imagen) {
+		      this.imagen = imagen;
+		   }
+
+		   public Image getImagen() {
+		      return imagen;
+		   }
+		}
+
